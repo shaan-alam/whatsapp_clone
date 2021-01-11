b5c10baaa82309d2377c77cbed526809f6bc1d03
Shaan Alam <shaanalam369@gmail.com>
2021-01-11 14:15:28 +0530

    implemented react router dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import HomeScreen from "./components/HomeScreen";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
-      <Chat />
+      <Router>
+        <Switch>
+          <Route path="/rooms/:id">
+            <Chat />
+          </Route>
+          <Route path="/">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/HomeScreen/index.js
@@ -0,0 +1,18 @@
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+      <div className="homescreen__content">
+        <div className="homescreen__img"></div>
+        <h1>Keep your phone connected</h1>
+        <p>
+          WhatsApp connects to your phone to sync messages. To reduce data
+          usage, connect your phone to Wi-Fi.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
