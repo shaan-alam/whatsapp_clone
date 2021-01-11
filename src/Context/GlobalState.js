8d87b8a5bd63df18e23f6b4d5ac6e6823d88937b
Shaan Alam <shaanalam369@gmail.com>
2021-01-11 18:01:05 +0530

    Context API set up

---
+++ src/Context/GlobalState.js
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import { auth } from "../firebase/config";
+import firebase from "firebase";
+
+export const Context = createContext({});
+
+export default function GlobalContext({ children }) {
+  const [user, setUser] = useState({});
+
+  const loginWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        setUser(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const value = {
+    user,
+    loginWithGoogle,
+  };
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalContext from "./Context/GlobalState";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalContext>
+      <App />
+    </GlobalContext>
   </React.StrictMode>,
   document.getElementById("root")
 );
